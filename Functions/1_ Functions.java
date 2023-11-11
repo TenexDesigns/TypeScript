040c76b6a0c1cb18c72843cafedbf8b1c8edce95
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-11 12:39:05 +0300

    Update 1_ Functions.java

--- Functions/1_ Functions.java
+++ Functions/1_ Functions.java
@@ -169,3 +169,189 @@ const result = square(5);
 console.log(result); // Output: 25
 
 const
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------Certainly! Here are some examples of functions in TypeScript:
+
+1. **Basic Function:**
+   ```typescript
+   function greet(name: string): string {
+       return `Hello, ${name}!`;
+   }
+
+   const greeting = greet("John");
+   console.log(greeting); // Output: Hello, John!
+   ```
+
+2. **Function with Optional Parameter:**
+   ```typescript
+   function introduce(firstName: string, lastName?: string): string {
+       if (lastName) {
+           return `Hello, ${firstName} ${lastName}!`;
+       } else {
+           return `Hello, ${firstName}!`;
+       }
+   }
+
+   const fullIntroduction = introduce("John", "Doe");
+   const partialIntroduction = introduce("Jane");
+   ```
+
+3. **Function with Default Parameter:**
+   ```typescript
+   function multiply(a: number, b: number = 2): number {
+       return a * b;
+   }
+
+   const result1 = multiply(3); // Output: 6
+   const result2 = multiply(3, 4); // Output: 12
+   ```
+
+4. **Rest Parameters:**
+   ```typescript
+   function sum(...numbers: number[]): number {
+       return numbers.reduce((total, num) => total + num, 0);
+   }
+
+   const total = sum(1, 2, 3, 4, 5); // Output: 15
+   ```
+
+5. **Function Expression:**
+   ```typescript
+   const square = function (x: number): number {
+       return x * x;
+   };
+
+   const squaredValue = square(4); // Output: 16
+   ```
+
+6. **Arrow Function (Lambda Expression):**
+   ```typescript
+   const add = (a: number, b: number): number => a + b;
+
+   const sumResult = add(5, 3); // Output: 8
+   ```
+
+7. **Higher-Order Function:**
+   ```typescript
+   function operation(x: number, y: number, operationFn: (a: number, b: number) => number): number {
+       return operationFn(x, y);
+   }
+
+   const addResult = operation(3, 4, (a, b) => a + b); // Output: 7
+   const multiplyResult = operation(3, 4, (a, b) => a * b); // Output: 12
+   ```
+
+8. **Function Overloading:**
+   ```typescript
+   function greetPerson(person: { name: string }): string;
+   function greetPerson(name: string): string;
+
+   function greetPerson(param: string | { name: string }): string {
+       if (typeof param === 'string') {
+           return `Hello, ${param}!`;
+       } else {
+           return `Hello, ${param.name}!`;
+       }
+   }
+
+   const result1 = greetPerson("John"); // Output: Hello, John!
+   const result2 = greetPerson({ name: "Jane" }); // Output: Hello, Jane!
+   ```
+
+These examples cover various aspects of functions in TypeScript, including optional parameters, default parameters, rest parameters, function expressions, arrow functions, higher-order functions, and function overloading.
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+  The article covers various aspects of functions in TypeScript. Here's a summary of the topics covered:
+
+1. **Defining a Function:**
+   - Functions are introduced as the building blocks of readable, maintainable, and reusable code.
+   - A function is described as a set of statements to perform a specific task, organizing the program into logical blocks.
+   - The function declaration includes the function's name, return type, and parameters.
+
+2. **Calling a Function:**
+   - The process of calling a function to execute it is termed "function invocation."
+   - The syntax for calling a function is demonstrated.
+
+3. **Returning Functions:**
+   - Functions may return a value along with control back to the caller.
+   - The syntax for returning functions is provided.
+
+4. **Parameterized Function:**
+   - Parameters are explained as a mechanism for passing values to functions.
+   - Two ways of passing arguments to a function (Call by Value and Call by Pointer) are mentioned.
+
+5. **Positional Parameters:**
+   - The concept of positional parameters is introduced, along with an example.
+
+6. **Optional Parameters:**
+   - Optional parameters are discussed, allowing arguments to be optional during function execution.
+   - An example of a function with an optional parameter is provided.
+
+7. **Rest Parameters:**
+   - Rest parameters, similar to variable arguments in other languages, are explained.
+   - The syntax and an example of a function with rest parameters are presented.
+
+8. **Default Parameters:**
+   - Function parameters can have default values.
+   - The syntax and an example of a function with default parameters are given.
+
+9. **Anonymous Function:**
+   - Anonymous functions, which are not bound to an identifier, are introduced.
+   - Examples of simple and parameterized anonymous functions are provided.
+
+10. **Function Expression vs. Function Declaration:**
+    - The difference between function expression and function declaration is explained.
+    - Function declarations are parsed before execution, while function expressions are parsed only when encountered during execution.
+
+11. **The Function Constructor:**
+    - TypeScript's support for defining a function with the built-in JavaScript constructor `Function()` is mentioned.
+    - An example of using the Function constructor is provided.
+
+12. **Recursion:**
+    - Recursion as a technique for iterating over an operation by having a function call itself is discussed.
+    - Examples of recursive functions are given.
+
+13. **Lambda Functions:**
+    - Lambda functions, also known as arrow functions, are introduced.
+    - Syntax, examples of lambda expressions and statements, and syntactic variations are provided.
+
+14. **Function Overloads:**
+    - TypeScript's support for function overloading is explained.
+    - Steps for overloading a function and an example with multiple function signatures are provided.
+
+The article covers a comprehensive range of topics related to TypeScript functions, from basic definitions to advanced concepts like recursion, lambda functions, and function overloads.
+
+
+  
+
+
+
+
+
+
+  
