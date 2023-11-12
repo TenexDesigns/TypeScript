c98fe5eee85a52f3c3f2e511fe0cc0592ea0b0dc
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-12 17:38:01 +0300

    Update Array_Methods and Transversal.java

--- Arrays/Array_Methods and Transversal.java
+++ Arrays/Array_Methods and Transversal.java
@@ -84,4 +84,130 @@ These are just a few examples of array traversal and methods in TypeScript. Each
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+Array traversal and manipulation are fundamental aspects of programming with arrays in TypeScript. Here are some of the common methods available to work with arrays:
+
+**Array Traversal:**
+
+1. **forEach**: This method performs the provided function once for each array element.
+    ```typescript
+    let arr = [1, 2, 3, 4, 5];
+    arr.forEach(function (value) {
+        console.log(value);
+    });
+    ```
+    This will log each value in the array to the console.
+
+**Array Methods:**
+
+1. **concat()**: This method is used to merge two or more arrays and returns a new array.
+    ```typescript
+    let arr1 = [1, 2, 3];
+    let arr2 = [4, 5, 6];
+    let arr3 = arr1.concat(arr2);
+    console.log(arr3); // Output: [1, 2, 3, 4, 5, 6]
+    ```
+    [Source 2](https://blog.canopas.com/typescript-array-methods-and-their-usages-daa8d498b4fd)
+
+2. **push()**: This method adds one or more elements to the end of an array and returns the new length of the array.
+    ```typescript
+    let arr = [1, 2, 3];
+    arr.push(4);
+    console.log(arr); // Output: [1, 2, 3, 4]
+    ```
+    [Source 4](https://www.copycat.dev/blog/typescript-array/)
+
+3. **pop()**: This method removes the last element from an array and returns that element.
+    ```typescript
+    let arr = [1, 2, 3, 4];
+    let last = arr.pop();
+    console.log(last); // Output: 4
+    console.log(arr); // Output: [1, 2, 3]
+    ```
+    [Source 4](https://www.copycat.dev/blog/typescript-array/)
+
+4. **reverse()**: This method reverses the order of the elements in an array.
+    ```typescript
+    let arr = [1, 2, 3, 4];
+    arr.reverse();
+    console.log(arr); // Output: [4, 3, 2, 1]
+    ```
+    [Source 4](https://www.copycat.dev/blog/typescript-array/)
+
+5. **toString()**: This method converts an array into a string and returns the resulting string.
+    ```typescript
+    let arr = [1, 2, 3, 4];
+    let str = arr.toString();
+    console.log(str); // Output: "1,2,3,4"
+    ```
+    [Source 20](https://marketsplash.com/tutorials/typescript/typescript-array/)
+
+6. **valueOf()**: This method returns the primitive value of the specified array object.
+    ```typescript
+    let arr = [1, 2, 3, 4];
+    let value = arr.valueOf();
+    console.log(value); // Output: [1, 2, 3, 4]
+    ```
+    [Source 20](https://marketsplash.com/tutorials/typescript/typescript-array/)
+
+7. **map()**: This method creates a new array with the results of calling a provided function on every element in the calling array.
+    ```typescript
+    let arr = [1, 2, 3, 4];
+    let newArr = arr.map(num => num * 2);
+    console.log(newArr); // Output: [2, 4, 6, 8]
+    ```
+    [Source 20](https://marketsplash.com/tutorials/typescript/typescript-array/)
+
+8. **filter()**: This method creates a new array with all elements that pass the test implemented by the provided function.
+    ```typescript
+    let arr = [1, 2, 3, 4];
+    let filteredArr = arr.filter(num => num > 2);
+    console.log(filteredArr); // Output: [3, 4]
+    ```
+    [Source 20](https://marketsplash.com/tutorials/typescript/typescript-array/)
+
+9. **reduce()**: This method applies a function against an accumulator and each element in the array (from left to right) to reduce it to a single output value.
+    ```typescript
+    let arr = [1, 2, 3, 4];
+    let sum = arr.reduce((total, value) => total + value, 0);
+    console.log(sum); // Output: 10
+    ```
+    [Source 20](https://marketsplash.com/tutorials/typescript/typescript-array/)
+
+10. **find()**: This method returns the value of the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
+    ```typescript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
   ....
