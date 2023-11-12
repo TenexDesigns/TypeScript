89761f1928acd86aba5821b213c9e6e4c4f40c70
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-12 16:29:59 +0300

    Create 2_Numbers.java

---
+++ Numbers/2_Numbers.java
@@ -0,0 +1,36 @@
+The provided text explains various aspects of numbers in TypeScript, with a focus on the Number object. Here's a summary of the covered topics:
+
+1. **Introduction to Number Object:**
+   - TypeScript supports numeric values as Number objects.
+   - The Number class acts as a wrapper for numeric literals, treating them as objects.
+   - Syntax for creating a Number object: `var var_name = new Number(value)`
+   - If a non-numeric argument is passed to the constructor, it returns NaN.
+
+2. **Number Properties:**
+   - Describes properties of the Number object, including MAX_VALUE, MIN_VALUE, NaN, NEGATIVE_INFINITY, POSITIVE_INFINITY, prototype, and constructor.
+   - Provides examples demonstrating the usage of these properties.
+
+3. **NaN Example:**
+   - Illustrates an example of setting a variable to NaN based on a condition.
+
+4. **Prototype Example:**
+   - Demonstrates the use of the prototype property to assign new properties and methods to the Number object.
+
+5. **Number Methods:**
+   - Lists commonly used methods of the Number object, including toExponential, toFixed, toLocaleString, toPrecision, toString, and valueOf.
+
+6. **Method Examples:**
+   - Provides examples for each method:
+     - toExponential: Converts a number to exponential notation.
+     - toFixed: Formats a number with a specific number of digits after the decimal.
+     - toLocaleString: Converts a number into a human-readable string using the locale.
+     - toPrecision: Returns a string representing the number to the specified precision.
+     - toString: Returns a string representing the specified number object.
+     - valueOf: Returns the primitive value of the specified number object.
+
+The examples include code snippets and the corresponding output for better understanding of each concept and method. The article covers the basics of the Number object, its properties, methods, and practical examples of their usage.
+
+
+
+
+  ......
