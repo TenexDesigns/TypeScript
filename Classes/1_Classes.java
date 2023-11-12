317e013120756dc57b50f086debc5647bb6eeba1
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-12 20:30:42 +0300

    Create 1_Classes.java

---
+++ Classes/1_Classes.java
@@ -0,0 +1,166 @@
+The provided information explains the concept of classes in TypeScript. Here's a summary:
+
+### Creating Classes:
+
+To declare a class in TypeScript, use the `class` keyword, followed by the class name. The class definition can include fields, constructors, and functions (methods).
+
+Example:
+```typescript
+class Car {
+  // field
+  engine: string;
+
+  // constructor
+  constructor(engine: string) {
+    this.engine = engine;
+  }
+
+  // method
+  disp(): void {
+    console.log("Engine is: " + this.engine);
+  }
+}
+```
+
+### Creating Instance Objects:
+
+To create an instance of a class, use the `new` keyword followed by the class name. You can pass arguments to the class constructor if it is defined.
+
+Example:
+```typescript
+var obj = new Car("Engine 1");
+```
+
+### Accessing Attributes and Functions:
+
+Use the dot notation (`obj.field_name` or `obj.function_name()`) to access attributes and functions of a class through the object.
+
+Example:
+```typescript
+console.log("Reading attribute value Engine as: " + obj.engine);
+obj.disp();
+```
+
+### Class Inheritance:
+
+TypeScript supports class inheritance using the `extends` keyword. Child classes inherit properties and methods (except private members and constructors) from the parent class.
+
+Example:
+```typescript
+class Shape {
+  Area: number;
+
+  constructor(a: number) {
+    this.Area = a;
+  }
+}
+
+class Circle extends Shape {
+  disp(): void {
+    console.log("Area of the circle: " + this.Area);
+  }
+}
+
+var obj = new Circle(223);
+obj.disp();
+```
+
+### Method Overriding:
+
+Method overriding allows a child class to redefine a method from its superclass. Use the `super` keyword to invoke the superclass version of the method.
+
+Example:
+```typescript
+class PrinterClass {
+  doPrint(): void {
+    console.log("doPrint() from Parent called…");
+  }
+}
+
+class StringPrinter extends PrinterClass {
+  doPrint(): void {
+    super.doPrint();
+    console.log("doPrint() is printing a string…");
+  }
+}
+
+var obj = new StringPrinter();
+obj.doPrint();
+```
+
+### Static Keyword:
+
+The `static` keyword can be applied to class members. Static members are referenced by the class name and retain their values until the program finishes execution.
+
+Example:
+```typescript
+class StaticMem {
+  static num: number;
+
+  static disp(): void {
+    console.log("The value of num is " + StaticMem.num);
+  }
+}
+
+StaticMem.num = 12;
+StaticMem.disp();
+```
+
+### The `instanceof` Operator:
+
+The `instanceof` operator checks if an object belongs to a specified type.
+
+Example:
+```typescript
+class Person { }
+var obj = new Person();
+var isPerson = obj instanceof Person;
+console.log("obj is an instance of Person: " + isPerson);
+```
+
+### Data Hiding:
+
+Data hiding or encapsulation can be achieved using access modifiers: `public`, `private`, and `protected`. These modifiers control the visibility of class members.
+
+Example:
+```typescript
+class Encapsulate {
+  str: string = "hello";
+  private str2: string = "world";
+}
+
+var obj = new Encapsulate();
+console.log(obj.str); // accessible
+// console.log(obj.str2); // compilation error, as str2 is private
+```
+
+### Classes and Interfaces:
+
+Classes can implement interfaces, ensuring that the class includes the specified properties of the interface.
+
+Example:
+```typescript
+interface ILoan {
+  interest: number;
+}
+
+class AgriLoan implements ILoan {
+  interest: number;
+  rebate: number;
+
+  constructor(interest: number, rebate: number) {
+    this.interest = interest;
+    this.rebate = rebate;
+  }
+}
+
+var obj = new AgriLoan(10, 1);
+console.log("Interest is: " + obj.interest + " Rebate is: " + obj.rebate);
+```
+
+These concepts provide a foundation for creating well-organized and maintainable code using classes in TypeScript.
+
+
+
+
+  .....
