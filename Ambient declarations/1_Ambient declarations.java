7716dac0d1a1997decc870ba7efd7791d7433a04
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-13 10:10:41 +0300

    Update 1_Ambient declarations.java

--- Ambient declarations/1_Ambient declarations.java
+++ Ambient declarations/1_Ambient declarations.java
@@ -1,6 +1,8 @@
 Sure, here is a summary of ambient declarations in TypeScript with code samples:
 
-**Ambient declarations** are a way to tell the TypeScript compiler that the actual source code exists elsewhere. They are used to provide type information for JavaScript libraries or other external sources that are not written in TypeScript. This allows you to use these libraries in your TypeScript code without having to rewrite them in TypeScript.
+**Ambient declarations** are a way to tell the TypeScript compiler that the actual source code exists elsewhere. 
+  They are used to provide type information for JavaScript libraries or other external sources that are not written in TypeScript.
+  This allows you to use these libraries in your TypeScript code without having to rewrite them in TypeScript.
 
 **Defining Ambient Declarations:**
 
@@ -50,5 +52,151 @@ console.log(element.text());
 **Conclusion:**
 
 Ambient declarations are a powerful tool for using JavaScript libraries in TypeScript projects. They provide type safety, better IntelliSense, and easier integration with TypeScript projects.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------
+
+
+
+
+
+  Ambient declarations in TypeScript are used to describe the shape and structure of existing JavaScript code, especially when integrating TypeScript with libraries or code that was not originally written in TypeScript. These declarations help the TypeScript compiler understand the types and signatures of variables, functions, and classes that exist outside the TypeScript codebase.
+
+### *.d.ts Files:
+
+- **Definition Files (.d.ts):**
+  - Files with the extension `.d.ts` are called definition files in TypeScript. They are used to provide ambient declarations for existing JavaScript code.
+
+### Purpose of *.d.ts Files:
+
+1. **Type Declarations:**
+   - They primarily contain type declarations, including interfaces, classes, functions, and variable types.
+
+2. **TypeScript Compatibility:**
+   - They enable TypeScript to work with JavaScript libraries and code by providing type information.
+
+### How to Use *.d.ts Files in TypeScript:
+
+1. **Reference in Code:**
+   - Use the `/// <reference path="filename.d.ts" />` syntax at the top of your TypeScript file to reference the definition file.
+
+   ```typescript
+   /// <reference path="someLibrary.d.ts" />
+   ```
+
+2. **Include in tsconfig.json:**
+   - Include the `.d.ts` files in the `files` or `include` section of your `tsconfig.json` if you are using TypeScript project configuration.
+
+   ```json
+   {
+     "files": [
+       "app.ts",
+       "someLibrary.d.ts"
+     ]
+   }
+   ```
+
+### Example of Ambient Declaration:
+
+Suppose you have a JavaScript library with the following code:
+
+```javascript
+// someLibrary.js
+function greet(name) {
+    return "Hello, " + name;
+}
+```
+
+You can create an ambient declaration file to provide type information for this library:
+
+```typescript
+// someLibrary.d.ts
+declare function greet(name: string): string;
+```
+
+Now, you can use this library in your TypeScript code:
+
+```typescript
+/// <reference path="someLibrary.d.ts" />
+
+const result = greet("John");
+console.log(result);
+```
+
+### What Goes in *.d.ts Files:
+
+1. **Interfaces:**
+   - Declare interfaces that describe the shape of objects or classes.
+
+   ```typescript
+   interface Person {
+       name: string;
+       age: number;
+   }
+   ```
+
+2. **Functions:**
+   - Declare functions and their signatures.
+
+   ```typescript
+   declare function greet(name: string): string;
+   ```
+
+3. **Variables:**
+   - Declare global variables and their types.
+
+   ```typescript
+   declare const version: number;
+   ```
+
+4. **Classes:**
+   - Declare the structure of classes.
+
+   ```typescript
+   declare class Car {
+       start(): void;
+       stop(): void;
+   }
+   ```
+
+### Implementation in *.d.ts Files:
+
+Typically, `.d.ts` files focus on declarations rather than implementations. They are meant to describe the types and structures without providing the actual implementation. If you need to include implementation details, you would usually do so in regular TypeScript files (.ts).
+
+In summary, ambient declarations in TypeScript (.d.ts files) are used to describe the types and structures of existing JavaScript code. They are primarily focused on providing type information for TypeScript while interacting with non-TypeScript code. Implementation details are usually kept separate in regular `.ts` files.
+
+
+
+  ...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
